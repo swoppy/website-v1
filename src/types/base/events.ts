f8446e531fdb79fda792af23446857ca43ebb038
swoppy <json.barba@gmail.com>
2020-06-14 20:51:30 +0800

    Added type def for div and svg elements

--- src/types/base/events.ts
+++ src/types/base/events.ts
@@ -1,2 +1,3 @@
 export type DivClickEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
 export type DivFuncEventType = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+export type SvgFuncEventType = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => void;
